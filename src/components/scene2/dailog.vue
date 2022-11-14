610e44a201c4f4463641333ae493fea4ad5a2679
Kerocate <kerocate_moe@outlook.com>
2022-11-15 07:48:29 +0800

    音乐播放修复

--- src/components/scene2/dailog.vue
+++ src/components/scene2/dailog.vue
@@ -6,7 +6,7 @@
 #dailog {
     position: fixed;
     width: 90%;
-    height: 10rem;
+    height: 25vh;
     margin: 0 5%;
     padding: 0 3vw;
     color: white;
@@ -14,7 +14,14 @@
     background-color: #000000;
     z-index: 1;
     box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.5);
-    font-size: 2rem;
+    font-size: 10vh;
+}
+
+@media (max-aspect-ratio: 1/1) {
+    #dailog {
+        height: 40vw;
+        font-size: 10vw;
+    }
 }
 </style>
 
@@ -24,59 +31,34 @@ import { onMounted, onUnmounted } from "vue";
 import { ScrollTrigger } from "gsap/all";
 import * as mm from "@magenta/music/es6";
 import type { NoteSequence } from "@magenta/music/es6";
+
 let midiData: null | NoteSequence = null;
 const player = new mm.SoundFontPlayer(
     "https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus"
 );
 
-mm.urlToNoteSequence("/src/assets/midis/YourBestFriend.mid")
-    .then((data) => {
-        console.log("loading complete");
-        player
-            .loadSamples(data)
-            .then(() => {
-                player.stop();
-                player.resumeContext();
-                midiData = data;
-                console.log("ready to play");
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    })
-    .catch((e) => {
-        console.log(e);
-    });
-
 onMounted(() => {
+    mm.urlToNoteSequence("/src/assets/midis/YourBestFriend.mid").then(
+        value => {
+            console.log(value);
+            player.loadSamples(value).then(
+                () => {
+                    console.log(
+                        player.getPlayState(),
+                        player.isPlaying()
+                    );
+                    player.stop();
+                    player.resumeContext();
+                    midiData = value;
+                }
+            )
+        }
+    )
     ScrollTrigger.defaults({
         trigger: "#scene2",
         toggleActions: "restart pause revers pause",
         scrub: 0.1,
     });
-    gsap.set("#dailog", {
-        yPercent: 0,
-    });
-    let show = gsap
-        .timeline({
-            scrollTrigger: {
-                // pin: true,
-                start: "5% bottom",
-                end: "20% bottom",
-                onEnter: () => {
-                    if (midiData && !player.isPlaying()) {
-                        console.log('enter play')
-                        player.resumeContext();
-                        player.start(midiData!);
-                    }
-                },
-            },
-        })
-        .to("#dailog", {
-            yPercent: -120,
-            z: 1,
-            scale: 1,
-        });
     gsap.matchMedia().add(
         {
             isMobile: "(max-aspect-ratio: 1/1)",
@@ -85,48 +67,79 @@ onMounted(() => {
         (context) => {
             let { isMobile } = context.conditions;
             // console.log(context.conditions);
-            let dailog = gsap
+            gsap.set("#dailog", {
+                yPercent: 0,
+            });
+            let show = gsap
                 .timeline({
                     scrollTrigger: {
-                        start: "20% bottom",
-                        end: "60% bottom",
-                        onLeave: () => {
-                            player.stop();
-                        },
-                        onUpdate: () => {
-                            if (!player.isPlaying()) {
-                                console.log('play')
+                        start: "5% bottom",
+                        end: "20% bottom",
+                        onEnter: () => {
+                            if (midiData && !player.isPlaying()) {
+                                console.log('enter play');
                                 player.resumeContext();
                                 player.start(midiData!);
                             }
                         },
                     },
                 })
-                .to("#dailog", { text: "", fontSize: isMobile ? "10vw" : "10vh" })
+                .to("#dailog", {
+                    yPercent: -120,
+                    z: 1,
+                    scale: 1,
+                });
+            let dailog = gsap
+                .timeline({
+                    scrollTrigger: {
+                        start: "20% bottom",
+                        end: "60% bottom",
+                        scrub: isMobile ? .1 : 1,
+                        onUpdate: () => {
+                            if (midiData && !player.isPlaying()) {
+                                console.log('enter play');
+                                player.resumeContext();
+                                player.start(midiData!);
+                            }
+                        },
+                        onLeave: () => {
+                            player.stop();
+                        }
+                    },
+                    defaults: {
+                        ease: 'power4.inOut'
+                    }
+                })
+                .to("#dailog", { text: "" })
+                .to("#dailog", { text: "", fontSize: isMobile ? "10vw" : "7.5vh" })
                 .to("#dailog", { text: "你肯定很疑惑为什么我在这里..." })
-                .to("#dailog", { text: "", fontSize: isMobile ? "12vw" : "12vh" })
+
+                .to("#dailog", { text: "" })
+                .to("#dailog", { text: "", fontSize: isMobile ? "12vw" : "11vh" })
                 .to("#dailog", { text: "哇哈哈哈！！！！" })
                 .to("#dailog", { text: "......其实我也不知道" })
-                .to("#dailog", { text: "", fontSize: isMobile ? "6vw" : "8vh" })
+
+                .to("#dailog", { text: "" })
+                .to("#dailog", { text: "", fontSize: isMobile ? "7vw" : "5.5vh" })
                 .to("#dailog", {
                     text: "如你所见网站用了很多版权素材，很良心的在站点末尾做了感谢与声明。",
                 })
-                .to("#dailog", { text: "", fontSize: isMobile ? "8vw" : "8vh" })
+
+                .to("#dailog", { text: "" })
+                .to("#dailog", { text: "", fontSize: isMobile ? "8vw" : "5.5vh" })
                 .to("#dailog", {
                     text: "作为非盈利以梦想而凝聚的小组，显然知道版权的重要。",
                 })
+
                 .to("#dailog", { text: "" })
-                .to("#dailog", {
-                    text: "为啥要我解释Fair use的事情？？？有问题Github会解决的。",
-                    fontSize: isMobile ? "6vw" : "8vh",
-                })
-                .to("#dailog", { text: "", fontSize: isMobile ? "12vw" : "12vh" })
+                .to("#dailog", { text: "", fontSize: isMobile ? "7vw" : "5.5vh" })
+                .to("#dailog", { text: "为啥要我解释Fair use的事情？？？有问题Github会解决的。" })
+
+                .to("#dailog", { text: "" })
+                .to("#dailog", { text: "", fontSize: isMobile ? "10vw" : "11vh" })
                 .to("#dailog", { text: "好了，我该走了......" })
                 .to("#dailog", { yPercent: 0 });
         }
     );
 });
-// onUnmounted(() => {
-//     player.stop();
-// })
 </script>
